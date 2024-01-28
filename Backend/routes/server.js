ae11f0f7c4b472c499025e22e2647062e86650a2
iamvijayragav <iamvijayragav@gmail.com>
2024-01-28 08:01:15 +0530

    main server copy

--- Backend/routes/server.js
+++ Backend/routes/server.js
@@ -1,27 +1,97 @@
-
-const express = require("express");
-const app = express()
-// const mongoose = require("mongoose");
-var bodyParser = require("body-parser");
-var cors = require('cors');
-
-const { login,FetchProject, FetchUser ,AddingProject, AddUser} = require('../Controller/LoginController');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const app = express();
+const port = 8081;
+const { login, FetchProject, FetchUser, AddingProject, AddUser } = require('../Controller/LoginController');
 const Project = require('../modals/Project');
 const User = require("../modals/UserInfo");
-const AddProject = require('../modals/AddProject'); 
+const mongoUrl = "mongodb://localhost:27017/ProjectUserDetailDatabase";
+const FactoryModel = require('../modals/FactoryModel');
 
-const {connectToDatabase, mongoose} = require("../Controller/ConnectToDatabase");
+
+mongoose.connect(mongoUrl);
+
+const mainDbConnection = mongoose.connection;
+
+mainDbConnection.on('error', (err) => {
+    console.error(`Error connecting to Master database: ${err}`);
+});
+
+mainDbConnection.once('open', () => {
+    console.log('Connected to Mater database');
+});
 
 app.use(cors());
-app.use(bodyParser.urlencoded({
-    extended: true
-}))
-app.use(express.json());
+app.use(bodyParser.json());
 
-const mongoUrl = "mongodb://localhost:27017/ProjectUserDetailDatabase";
-mongoose.connect(mongoUrl)
-    .then(() => { console.log("connected to database"); })
-    .catch((e) => console.log(e));
+const createConnection = (dbName) => {
+    return mongoose.createConnection(`mongodb://localhost:27017/${dbName}`);
+};
+
+let currentTempDbConnection = null;
+app.get("/api/dynamic-switch/:db", async (req, res) => {
+    const { db } = req.params;
+
+    if (currentTempDbConnection) {
+        currentTempDbConnection.close();
+        console.log('Closed current temporary database connection');
+    }
+
+    currentTempDbConnection = createConnection(db);
+
+    currentTempDbConnection.on('error', (err) => {
+        console.error(`Error connecting to dynamic database ${db}: ${err}`);
+        res.status(500).json({ error: `Error connecting to dynamic database ${db}` });
+    });
+
+    currentTempDbConnection.once('open', () => {
+        console.log(`Connected to dynamic database: ${db}`);
+        res.status(200).json({ message: `Connected to dynamic database: ${db}` });
+    });
+});
+
+app.get("/api/dynamic-category/:category/:db", async (req, res) => {
+    const { category, db } = req.params;
+    if (!currentTempDbConnection) {
+        currentTempDbConnection = createConnection(db);
+    }
+
+    const DocumentSchema = require("../modals/Document");
+    // const DocumentContent = currentTempDbConnection.model("Document_Content", documentContent);
+    const DocumentContent = FactoryModel(DocumentSchema, currentTempDbConnection, "Document_Content");
+
+    const CategorySchema = require('../modals/CategoryModal');
+
+    const CategoryContent = FactoryModel(CategorySchema(category), currentTempDbConnection, category);
+
+    const StakeHolderSchema = require("../modals/StakeHolder");
+
+    const StakeHolderContent = FactoryModel(StakeHolderSchema, currentTempDbConnection, "Stake_Holder");
+
+    try {
+        const CategoryData = await CategoryContent.find({});
+        const DocumentData = await DocumentContent.find({});
+        const StakeHolderData = await StakeHolderContent.find({});
+
+        const combinedData = CategoryData.map(categoryItem => {
+            const matchingDocument = DocumentData.find(documentItem => documentItem.New_SDRL_Code === categoryItem.New_SDRL_Code);
+            const matchingStakeHolder = StakeHolderData.find(stakeItem => stakeItem.New_SDRL_Code === categoryItem.New_SDRL_Code);
+
+            return {
+                ...categoryItem.toObject(),
+                ...(matchingDocument ? matchingDocument.toObject() : {}),
+                ...(matchingStakeHolder ? matchingStakeHolder.toObject() : {})
+            };
+        });
+        res.status(200).json(combinedData);
+    } catch (err) {
+        console.error("Error in fetching category", err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
 
 app.post("/login", login);
 
@@ -46,28 +116,28 @@ app.post("/add-project", AddingProject);
 
 app.post('/add-user', AddUser)
 
-app.post("/api/fetch-user", FetchUser);
+app.get("/api/fetch-user", FetchUser);
 
-app.post("/api/fetch-project", FetchProject);
+app.get("/api/fetch-project", FetchProject);
 
 app.post("/api/delete-user", async (req, res) => {
     const { email } = req.body;
     try {
         const DeletedUser = await User.findOneAndDelete({ email });
         if (!DeletedUser) {
-            res.status(404).json({error:'User Not Found'})
+            res.status(404).json({ error: 'User Not Found' })
         }
         res.json(DeletedUser);
     } catch (err) {
-        res.status(500).json({error:" Error in delete user"})
-        
+        res.status(500).json({ error: " Error in delete user" })
+
     }
 })
 
 app.post("/api/upload-access", async (req, res) => {
     const { Access, projectCode, userId } = req.body;
     try {
-        const filter = {$and: [{ user_id : userId } , { project_code : projectCode }] }
+        const filter = { $and: [{ user_id: userId }, { project_code: projectCode }] }
         const update = {
             project_code: projectCode,
             user_id: userId,
@@ -79,12 +149,12 @@ app.post("/api/upload-access", async (req, res) => {
         res.status(200).json(UpdatedAccess);
     } catch (err) {
         console.log("Error in Giving Access", err);
-        res.send({error : "Error in Giving Permissions"})
+        res.send({ error: "Error in Giving Permissions" })
     }
 })
 
 
-app.post('/api/fetch-project-access', async (req, res) => {
+app.get('/api/fetch-project-access', async (req, res) => {
     try {
         const ProjectAccess = await Project.find({});
         res.status(200).json(ProjectAccess);
@@ -93,72 +163,7 @@ app.post('/api/fetch-project-access', async (req, res) => {
         res.status(500).json({ error: "Error in Fetching Access." })
     }
 });
-const DatabaseConnection = async (dbname) => {
-    try {
-        // Replace this connection URL with your actual MongoDB connection URL
-        await mongoose.connect(`mongodb://localhost:27017/${dbname}`, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
 
-        });
-        console.log(`Connected to dynamic database: ${dbname}`);
-    } catch (error) {
-        console.error(`Error connecting to dynamic database ${dbname}: ${error.message}`);
-        throw error;
-    }
-};
-// app.post("/api/dynamic-switch/:db/:category", async (req, res) => {
-//     const { db, category } = req.params;
-//     try {
-//         await DatabaseConnection(db);
-//         res.status(200).json(Data);
-//     } catch (err) {
-//         console.error("Error in Switch DB:", err);
-//         res.status(500).json({ error: 'Internal Server Error' });
-//     }
-// });
-
-
-
-
-
-
-const createConnection = (dbName) => {
-    return mongoose.createConnection(`mongodb://localhost:27017/${dbName}`);
-};
-
-app.post("/api/dynamic-switch/:db/:category", async (req, res) => {
-    const { db, category } = req.params;
-    console.log(db,category);
-
-    // Use the provided database name to create a dynamic connection
-    const dynamicDbConnection = createConnection(db);
-
-    dynamicDbConnection.on('error', (err) => {
-        console.error(`Error connecting to dynamic database ${db}: ${err}`);
-        res.status(500).json({ error: `Error connecting to dynamic database ${db}` });
-    });
-
-    dynamicDbConnection.once('open', () => {
-        console.log(`Connected to dynamic database: ${db}`);
-
-        const DynamicModel = dynamicDbConnection.model('DynamicModel', new mongoose.Schema({
-        }));
-
-        DynamicModel.find({}, (err, data) => {
-            if (err) {
-                console.error(`Error fetching data from dynamic database ${db}: ${err}`);
-                res.status(500).json({ error: `Error fetching data from dynamic database ${db}` });
-            } else {
-                res.status(200).json(data);
-            }
-            dynamicDbConnection.close();
-        });
-    });
-});
-
-
-
-app.listen(8081, () => {
-    console.log("server started");
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
